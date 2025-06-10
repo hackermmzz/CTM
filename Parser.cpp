8af9ae5ce3dd01fbc9c6a676ca1b12329d58d39d
hackermmzz <108677256+hackermmzz@users.noreply.github.com>
2025-06-10 16:45:29 +0800

    wlh

--- Parser.cpp
+++ Parser.cpp
@@ -21,8 +21,92 @@ Token& Parser::cur(){
 }
 
 void Parser::parse(){
-    //parse type_specifier
-    Parse_type_specifier();
+    while(cur()!=0){
+    //先解析类型
+    auto*ts=Parse_type_specifier();
+    //跳过指针
+    int beg=idx;
+    int predict=0;
+    while(nextn(predict)!=0&&nextn(predict)==Star)++predict;
+    //满足 type {*} id ( 肯定是函数了
+    if(nextn(predict)==ID&&nextn(predict+1)==Lpb){
+        auto*ret=Parse_function(ts);
+        program.p.push_back({1,ret});
+    }
+    else{
+        auto*ret=Parse_declaration(ts);
+        program.p.push_back({0,ret});
+    }
+    }
+}
+Function *Parser::Parse_function(TypeSpecifier *ts)
+{
+    Function*ret=new Function;
+    ret->retType0=*ts;
+    //解析指针
+    int beg=idx;
+    while(cur()==Star)++idx;
+    ret->pointer=idx-beg;
+    //
+    if(cur()!=ID){
+        Error("in line "<<cur().data<<" pos "<<cur().pos<<" function must have a name");
+    }
+    //
+    ret->id=cur().data;
+    ++idx;
+    //
+    if(cur()!=Lpb){
+        Error("in line "<<cur().data<<" pos "<<cur().pos<<" missing \"(\"");
+    }
+    ++idx;
+    //
+    if(cur()!=Rpb){
+        while(1){
+            //解析函数的参数
+            auto*r0=Parse_type_specifier();
+            auto*r1=Parse_declarator();
+            Declaration dt;
+            InitDeclarator idt;
+            idt.dt=*r1;
+            dt.dl.push_back(idt);
+            dt.ts=*r0;
+            ret->argument.push_back(dt);
+            if(cur()==Comma){
+                ++idx;
+                continue;
+            }
+            else break;
+        }
+    }
+    //
+    if(cur()!=Rpb){
+        Error("in line "<<cur().data<<" pos "<<cur().pos<<" missing \")\"");
+    }
+    ++idx;
+    ret->body=Parse_composed_statement();
+    return ret;
+}
+
+Declaration *Parser::Parse_declaration(TypeSpecifier *ts)
+{
+    Declaration*ret=new Declaration;
+    ret->ts=*ts;
+    //
+    while(1){
+        auto*r=Parse_initDeclarator();
+        ret->dl.push_back(*r);
+        if(cur()==Comma){
+            ++idx;
+        }
+        else if(cur()==Sem){
+            ++idx;
+            break;
+        }
+        else{
+            Error("in line "<<cur().line<<" pos "<<cur().pos<<" missing \";\"");
+        }
+    }
+    return ret;
 }
 
 TypeSpecifier *Parser::Parse_type_specifier()
@@ -108,43 +192,104 @@ StructInfo* Parser::Parse_struct_declaration_list(string name)
 Declarator *Parser::Parse_declarator()
 {
     Declarator*ret=new Declarator;
-    ret->pointer=0;
-    //
-    while(cur()==Star)++ret->pointer,++idx;
-    //
-    if(cur()==ID){
-        ret->dd.id=cur().data;
-        ret->dd.dt=0;
+    //先跳过所有的*
+    int beg=idx;
+    while(cur()!=0&&cur()==Star)++idx;
+    int end=idx;
+    if(cur()==Lpb){
         ++idx;
-    }
-    else if(cur()==Lpb){
-        ++idx;
-        ret->dd.dt=Parse_declarator();
+        Declarator*rr=Parse_declarator();
+        *ret=*rr;
         if(cur()!=Rpb){
-            Error("line:"<<cur().line<<" position:"<<cur().pos<<" must be \")\"");
+            Error("in line "<<cur().line<<" located at "<<cur().pos<<" miss \")\"");
         }
         ++idx;
-    }
-    //check if it is array
-    if(cur()==Lsb){
-        ++idx;
-        if(cur()!=INT_Literal){
-            Error("line:"<<cur().line<<" position:"<<cur().pos<<" must to be constant int");
-        }
-        int len=stoi(cur().data);
-        if(len==0){
-            Error("line:"<<cur().line<<" position:"<<cur().pos<<" the size of array must great than zero!");
-        }
-        ret->dd.arrayLen=len;
-        ++idx;
-        if(cur()!=Rsb){
-            Error("line:"<<cur().line<<" position:"<<cur().pos<<" must be \"]\" !");
-        }
+    }else if(cur()==ID){
+        ret->id=cur().data;
         ++idx;
     }else{
-        ret->dd.arrayLen=0;
+        Error("one declarator must have id to name it!"<<" in line "<<cur().line<<" located at "<<cur().pos);
+    }
+    //把类型放到最后一层
+    auto*tp=&(ret->tp);
+    while(tp->tp)tp=tp->tp;
+    //处理数组
+    vector<int>array;
+    while(cur()==Lsb){
+        ++idx;
+        if(cur()!=INT_Literal){
+            Error("array definition must give a sure integer constant!"<<" in line "<<cur().line<<" pos "<<cur().pos)
+        }
+        int siz=stoi(cur().data);
+        if(siz<=0){
+            Error("array size must great than 0"<<" in line "<<cur().line<<" pos "<<cur().pos)
+        }
+        ++idx;
+        if(cur()!=Rsb){
+            Error("in line "<<cur().line<<" pos "<<cur().pos<<" missing \')\"");
+        }
+        ++idx;
+        array.push_back(siz);
+    }
+    if(array.size()){
+        if(tp->array.size()||tp->pointer)tp->tp=new DeclaratorType,tp=tp->tp;
+        tp->array=array;
+    }
+    //处理指针
+    int cnt=end-beg;
+    if(tp->array.size()||tp->pointer)tp->tp=new DeclaratorType,tp=tp->tp;
+    if(cnt)tp->pointer=cnt;
+    //
+    return ret;
+}
+
+InitDeclarator *Parser::Parse_initDeclarator()
+{
+    InitDeclarator*ret=new InitDeclarator;
+    //解析declarator
+    auto*r=Parse_declarator();
+    ret->dt=*r;
+    if(cur()==Assign){
+        ++idx;
+        ret->init=Parse_initializer();
     }
     return ret;
+}
+
+Initializer *Parser::Parse_initializer()
+{
+    auto*ret=new Initializer;
+    if(cur()==Lcb){
+        auto*r=Parse_initializerList();
+        if(cur()!=Rcb){
+            Error("in line "<<cur().line<<" pos "<<cur().pos<<" missing \"}\"");
+        }
+        ++idx;
+        ret->list=r;
+    }
+    else ret->exp=Parse_assignment_expression();
+    return ret;
+}
+
+AssignExp *Parser::Parse_assignment_expression()
+{
+    
+}
+
+InitializerList *Parser::Parse_initializerList()
+{
+    auto*ret=new InitializerList;
+    auto*r=Parse_initializer();
+    ret->init.push_back(r);
+    while(cur()==Comma){
+        ++idx;
+        ret->init.push_back(Parse_initializer());
+    }
+}
+
+ComposedStatment *Parser::Parse_composed_statement()
+{
+    return nullptr;
 }
 
 bool CheckIsBaseType(const Token &tk)
--- Parser.h
+++ Parser.h
@@ -1,6 +1,7 @@
 #ifndef PARSER_H
 #define PARSER_H
 #include"Lexifier.h"
+#include<string>
 ///////////////////////////////
 struct StructInfo;
 struct TypeSpecifier;
@@ -8,7 +9,25 @@ struct Declaration;
 struct InitDeclarator;
 struct Declarator;
 struct Initializer;
+struct Program;
+struct Function;
+struct DeclaratorType;
+struct ComposedStatment;
+struct AssignExp;
+struct InitializerList;
 ///////////////////////////////
+struct InitializerList{
+    vector<Initializer*>init;
+};
+struct Initializer{
+    AssignExp*exp;
+    InitializerList*list;
+};
+struct DeclaratorType{
+    int pointer;
+    vector<int>array;
+    DeclaratorType*tp;
+};
 struct TypeSpecifier{
     bool _signed;
     int baseType;//mark down if it is a base type
@@ -16,12 +35,9 @@ struct TypeSpecifier{
     StructInfo*si;
 };
 struct Declarator{
-    int pointer;
-    struct DirectDeclarator{
-        string id;
-        Declarator*dt;
-        int arrayLen;
-    }dd;
+    //对于一个声明符,他要么是指针，要么是数组，要么什么也不是
+    string id;
+    DeclaratorType tp;
 };
 struct Declaration{
     TypeSpecifier ts;
@@ -37,20 +53,38 @@ struct StructInfo
     string id;
     vector<Declaration>member;
 };
-
+struct Function{
+    TypeSpecifier retType0;
+    int pointer;
+    string id;
+    vector<Declaration>argument;
+    ComposedStatment*body;
+};
+struct Program{
+    //declaration为0,function为1
+    vector<pair<int,void*>>p;
+};
 ///////////////////////////////
 struct Parser{
     int idx;
     vector<Token>&token;
+    Program program;
     Parser(vector<Token>&tk);
     Token& next();
     Token& nextn(int n);
     Token& cur();
     void parse();
+    Function*Parse_function(TypeSpecifier*ts);
+    Declaration*Parse_declaration(TypeSpecifier*ts);
     TypeSpecifier*Parse_type_specifier();
     pair<string,StructInfo*> Parse_Struct();
     StructInfo* Parse_struct_declaration_list(string name);
     Declarator* Parse_declarator();
+    InitDeclarator*Parse_initDeclarator();
+    Initializer*Parse_initializer();
+    AssignExp* Parse_assignment_expression();
+    InitializerList*Parse_initializerList();
+    ComposedStatment* Parse_composed_statement();
 };
 ///////////////////////////////
 bool CheckIsBaseType(const Token&tk);
